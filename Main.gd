14177d111271031516705135e304eff719dc332c
FabriDamazio <fabridamazio@gmail.com>
2021-09-19 12:10:45 -0300

    Remove Mob rotation

--- Main.gd
+++ Main.gd
@@ -35,7 +35,7 @@ func _on_MobTimer_timeout():
 	mob.position = $MobPath/MobSpawnLocation.position
 	# Add some randomness to the direction.
 	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+	#mob.rotation = direction
 	# Set the velocity (speed & direction).
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
