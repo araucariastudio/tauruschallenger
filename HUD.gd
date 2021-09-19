40c987bca4da01c0157986b692ba109436888e12
FabriDamazio <fabridamazio@gmail.com>
2021-09-19 12:27:27 -0300

    Add Araucaria Studio label

--- HUD.gd
+++ HUD.gd
@@ -23,11 +23,13 @@ func _on_MessageTimer_timeout():
 
 func _on_StartButton_pressed():
 	$StartButton.hide()
+	$Message2.hide()
 	emit_signal("start_game")
 
 func show_main_menu():
 	$Message.text = "Taurus\nChallenger"
 	$Message.show()
+	$Message2.show()
 	# Make a one-shot timer and wait for it to finish.
 	yield(get_tree().create_timer(1), "timeout")
 	$StartButton.show()
